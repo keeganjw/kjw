9a37515d4c71cd211b6434c0310ac902ea3816ee
Keegan Woodward <keeganjw@users.noreply.github.com>
2022-08-18 17:15:55 -0400

    Add admin list view, readme, and configured editing

---
+++ README.md
@@ -0,0 +1,26 @@
+# kjw.dev
+
+This is a repository for my personal site: [kjw.dev](https://kjw.dev)
+
+## Todo List
+
+### Remaining
+
+- [ ] Create admin published posts page
+- [ ] Create admin draft posts page
+- [ ] Finish preview page
+- [ ] Create separate layout and style for admin pages
+- [ ] Add user accounts
+- [ ] Add login page
+- [ ] Protect admin pages behind login
+- [ ] Add image upload ability to posts
+
+### Completed
+
+- [X] Fill edit post page with post data
+- [X] Create blog post list, view, and new post pages
+- [X] Add Showdown (markdown-to-html converter)
+- [X] Configure Mongoose/MongoDB
+- [X] Configure routing
+- [X] Configure Handlebars
+- [X] Configure Express
--- admin.js
+++ admin.js
@@ -5,53 +5,93 @@ const Post = require('./models/post');
 
 const markdown = new showdown.Converter();
 
-router.get('/', async (req, res) => {
-    const posts = await Post.find({ isPublished: true }).lean();
-    res.render('blog/list', { title: 'Blog', posts: posts });
+router.get('/published', async (req, res) => {
+	const posts = await Post.find({ isPublished: true }).lean();
+	res.render('admin/list', { title: 'Published Posts', posts: posts });
+});
+
+router.get('/drafts', async (req, res) => {
+	const posts = await Post.find({ isPublished: false }).lean();
+	res.render('admin/list', { title: 'Drafts', posts: posts });
 });
 
 router.post('/new', async (req, res) => {
-    let post = new Post({
-        title: req.body.title,
-        author: req.body.author,
-        slug: req.body.slug,
-        description: req.body.description,
-        article: req.body.article
-    });
+	let post = new Post({
+		title: req.body.title,
+		author: req.body.author,
+		slug: req.body.slug,
+		description: req.body.description,
+		article: req.body.article
+	});
 
-    try {
-        post.isPublished = true;
-        post = await post.save();
-        res.redirect(`/blog/${post.id}`);
-    }
-    catch (error) {
-        console.log(error);
-        res.render('admin/new', { title: 'Write New Post', post: post });
-    }
+	try {
+		post.isPublished = req.body.submit === 'publish' ? true : false;
+		post = await post.save();
+		
+		if(post.isPublished) {
+			res.redirect(`/blog/${post.slug}`);
+		}
+		else {
+			res.redirect(`/admin/preview/${post.slug}`);
+		}
+	}
+	catch (error) {
+		console.log(error);
+		res.render('admin/new', { title: 'Write New Post', post: post });
+	}
 });
 
 router.get('/new', (req, res) => {
-    res.render('admin/new', { title: 'Write New Post', post: new Post() });
+	res.render('admin/new', { title: 'Write New Post', post: new Post() });
 });
 
-router.get('/edit/:slug', (req, res) => {
-    res.render('admin/edit');
+router.get('/edit/:slug', async (req, res) => {
+	const slug = req.params.slug;
+
+	try {
+		let post = await Post.findOne({ slug: slug }).lean();
+		res.render('admin/edit', { title: post.title, post: post });
+	}
+	catch (error) {
+		res.send('Blog post not found for editing.');
+	}
+});
+
+router.post('/edit/:slug', async (req, res) => {
+	const slug = req.params.slug;
+	let update = {
+		title: req.body.title,
+		author: req.body.author,
+		slug: req.body.slug,
+		description: req.body.description,
+		article: req.body.article
+	}
+
+	// TODO: Change isPublished status here
+
+	try {
+		let post = await Post.findOneAndUpdate({ slug: slug }, update, { returnOriginal: false }).lean();
+		res.redirect(`/admin/preview/${post.slug}`);
+	}
+	catch (error) {
+		res.send('Blog post not found for editing.');
+	}
 });
 
 router.get('/preview/:slug', async (req, res) => {
-    const slug = req.params.slug;
+	const slug = req.params.slug;
 
-    try {
-        let post = await Post.findOne({ slug: slug }).lean();
-        post.article = markdown.makeHtml(post.article);
-        post.dateCreated = new Date(post.dateCreated).toDateString();
-        res.render('admin/preview', { title: post.title, post: post });
-    }
-    catch (error) {
-        res.send('Blog post not found.');
-    }
+	try {
+		let post = await Post.findOne({ slug: slug }).lean();
+		post.article = markdown.makeHtml(post.article);
+		post.dateCreated = new Date(post.dateCreated).toDateString();
+		res.render('admin/preview', { title: post.title, post: post });
+	}
+	catch (error) {
+		res.send('Blog post not found.');
+	}
 });
 
 module.exports = {
-    router
+	router
 };
