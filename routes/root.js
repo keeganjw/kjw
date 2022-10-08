c2c353e20aac985a89f5e1b99f883c5f0232dedd
Keegan Woodward <keeganjw@users.noreply.github.com>
2022-10-07 22:38:08 -0400

    Deleted about page, updated work orders page

--- routes/root.js
+++ routes/root.js
@@ -10,10 +10,6 @@ router.get('/', (req, res) => {
 	res.render('index', { title: 'Keegan Woodward' });
 });
 
-router.get('/about', (req, res) => {
-	res.render('about', { title: 'About' });
-});
-
 router.get('/contact', (req, res) => {
 	res.render('contact', { title: 'Contact' });
 });
